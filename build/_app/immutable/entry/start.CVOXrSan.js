a0f47c70b2f67d47638d11658e9f15d5506db211
Jakob Harteg <github.0javt@aleeas.com>
2024-12-07 21:12:17 +0100

    Revert "Fix: Update 404 handling to redirect to requested URL"
    This reverts commit 4c1f300a0c7472b45af68d37d9ffde5247afa6fc.

--- build/_app/immutable/entry/start.CVOXrSan.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.4X_fIvvZ.js";export{t as start};
--- src/routes/+error.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    export let status;
-    export let error;
-</script>
-
-<h1>{status}: {error.message}</h1>
-<p>Sorry, the page you are looking for does not exist.</p>
